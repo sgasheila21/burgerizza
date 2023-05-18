c212e47a223921098eeac60dbc7b85e887581023
FELINA SURYADI <felina.suryadi@binus.edu>
2023-05-19 02:02:36 +0700

    add customize order background - Felina

---
+++ resources/views/customize-order.blade.php
@@ -0,0 +1,12 @@
+@extends('template/layout-two')
+@section('title', 'CUSTOMIZE ORDER')
+
+<style>
+    body { 
+      background: linear-gradient(180deg, #D82911 16.8%, rgba(230, 149, 138, 0.453125) 81.25%, rgba(216, 41, 17, 0) 100%);
+    }
+</style>
+
+@section('sub-content')
+
+@endsection
