2885a6afd8611a0e87a9b78a1ab1076a5231d0c5
Pat <patrice.agustin@binus.ac.id>
2023-05-20 14:07:47 +0700

    customize order

--- resources/views/customize-order.blade.php
+++ resources/views/customize-order.blade.php
@@ -8,5 +8,36 @@
 </style>
 
 @section('sub-content')
+@for ($i = 0; $i < 5; $i++)
+<div class="accordion m-1 p-2" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading{{$i}}">
+      <button class="accordion-button collapsed p-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$i}}" aria-expanded="false" aria-controls="collapse{{$i}}">
+        Select Your Dough 
+      </button>
+    </h2>
+    <div id="collapse{{$i}}" class="accordion-collapse collapse" aria-labelledby="heading{{$i}}" data-bs-parent="#accordionExample">
+      @for ($i2 = 0; $i2 < 5; $i2++)
+      
+      <div class="accordion-body">
+        <div class="card" >
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">Item Name</p>
+            <div class="qty ">
+              <input type="number" class="count" name="qty" value="1">
+          </div>
+          </div>
+        </div>
+      </div>
+      @endfor
+    </div>
+  </div>
+</div>
+@endfor
+
+<div class="add-to-cart-div">
+  <button>Add To Cart</button>
+</div>
 
 @endsection
